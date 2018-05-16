60ae21c7795bf39efda4d18de216039ade02a558
MathieuBouvet <mathieubouvet42@gmail.com>
2018-05-16 20:51:57 +0200

    Add method isPosition in Tetrimino class

--- src/Model/Tetrimino.js
+++ src/Model/Tetrimino.js
@@ -6,6 +6,21 @@ class Tetrimino {
 		this.orientation = 0;
 		this.position = [];
 	}
+	/**
+	 * return true if the coordinates given in parameters match any block position of the tetramino
+	 * @param  integer  x coordinate
+	 * @param  integer  y coordinate
+	 * @return Boolean   
+	 */
+	isPosition(x,y){
+		for(let i=0 ; i<this.position.length ; i++){
+			let p = this.position[i];
+			if(p.posX === x && p.posY === y){
+				return true;
+			}
+		}
+		return false;
+	}
 }
 
 export default Tetrimino;
