b5a7475fd57728c60ba1197366802c5d9f95bc00
MathieuBouvet <mathieubouvet42@gmail.com>
2018-05-16 18:27:07 +0200

    Create basic Cell and style in red for dev

--- src/Cell.js
+++ src/Cell.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Cell.css";
 
-const Cell = (props) => (
-	
+const Cell = ({blockType}) => (
+	<div className={`block  ${blockType}`}></div>
 );
 
 export default Cell;
