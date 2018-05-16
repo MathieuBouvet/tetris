8bca1c7a685463846b919bda77c115a19101b676
MathieuBouvet <mathieubouvet42@gmail.com>
2018-05-16 20:46:13 +0200

    Add Shortcuts method for left, right, and down translation in Simple2DPosition

--- src/Model/Simple2DPosition.js
+++ src/Model/Simple2DPosition.js
@@ -7,6 +7,15 @@ class Simple2DPosition {
 	getTranslation(xShift, yShift){
 		return new Simple2DPosition(this.posX+xShift, this.posY+yShift);
 	}
+	getLeft(){
+		return this.getTranslation(-1,0);
+	}
+	getRight(){
+		return this.getTranslation(1,0);
+	}
+	getDown(){
+		return this.getTranslation(0,1);
+	}
 }
 
 export default Simple2DPosition;
