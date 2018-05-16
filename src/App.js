2a8b11cc4bd9d17df15ec5856658909a9caf4da7
MathieuBouvet <mathieubouvet42@gmail.com>
2018-05-16 18:28:20 +0200

    Create the game board in App and display the cells

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Cell from "./Cell";
+
+function createBoard(sizeFirst, sizeSecond) {
+  var arr = [];
+  for(var i=0 ; i<sizeFirst ; i++){
+    arr[i] = [];
+    for(var j=0; j<sizeSecond ; j++){
+      arr[i].push("empty");
+    }
+  }
+  return arr; 
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      board: createBoard(20,10),
+    }
+  }
   render() {
+    const { board } = this.state;
     return (
       <div className="App">
-
+        <div className="board">
+          { board.map( (row, rowIndex) => (
+            row.map( (elem, columnIndex) => (
+              <Cell key={rowIndex.toString()+columnIndex.toString()} blockType={elem} />
+            ))
+          ))}
+        </div>
       </div>
     );
   }
