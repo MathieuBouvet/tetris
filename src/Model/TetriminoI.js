0679da6882f5fe8b911d1c1fb4824abe41f5c87b
MathieuBouvet <mathieubouvet42@gmail.com>
2018-05-17 01:34:22 +0200

    Create TetriminoI

---
+++ src/Model/TetriminoI.js
@@ -0,0 +1,32 @@
+import Tetrimino from "./Tetrimino";
+import Simple2DPosition from "./Simple2DPosition";
+
+class TetriminoI extends Tetrimino {
+	constructor(){
+		super();
+		this.type = "I";
+		/*this.position = [
+			new Simple2DPosition(-1,3),
+			new Simple2DPosition(-1,4),
+			new Simple2DPosition(-1,5),
+			new Simple2DPosition(-1,6),
+		];*/
+		//TEST POSITION
+		this.position = [
+			new Simple2DPosition(6,3),
+			new Simple2DPosition(6,4),
+			new Simple2DPosition(6,5),
+			new Simple2DPosition(6,6),
+		];
+	}
+	getRotationMapping(){
+		return [
+		[[-1,2],[0,1],[1,0],[2,-1]],
+		[[2,1],[1,0],[0,-1],[-1,-2]],
+		[[1,-2],[0,-1],[-1,0],[-2,1]],
+		[[-2,-1],[-1,0],[0,1],[1,2]],
+		]
+	}
+}
+
+export default TetriminoI;
