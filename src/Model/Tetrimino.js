b62c9ef23a002d31f09a092b6723a45edad71a1e
MathieuBouvet <mathieubouvet42@gmail.com>
2018-05-16 20:50:44 +0200

    Base class for tetriminoes

--- src/Model/Tetrimino.js
+++ src/Model/Tetrimino.js
@@ -1,7 +1,11 @@
 import Simple2DPosition from "./Simple2DPosition";
 
 class Tetrimino {
-
+	constructor(tetriminoType){
+		this.type = tetriminoType;
+		this.orientation = 0;
+		this.position = [];
+	}
 }
 
 export default Tetrimino;
