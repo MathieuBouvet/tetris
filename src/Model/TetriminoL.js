7b022d5e3ff6e5e23fdc2362b125ac635049ba14
MathieuBouvet <mathieubouvet42@gmail.com>
2018-05-16 23:31:36 +0200

    Create TetriminoL, a child class of Tetrimino

---
+++ src/Model/TetriminoL.js
@@ -0,0 +1,34 @@
+import Tetrimino from "./Tetrimino";
+import Simple2DPosition from "./Simple2DPosition";
+
+class TetriminoL extends Tetrimino {
+	constructor(){
+		super();
+		this.type = "L";
+		/*this.position = [
+			new Simple2DPosition(-1,3),
+			new Simple2DPosition(-1,4),
+			new Simple2DPosition(-1,5),
+			new Simple2DPosition(-2,5),
+		];*/
+		//TEST POSITION
+		this.position = [
+			new Simple2DPosition(5,3),
+			new Simple2DPosition(5,4),
+			new Simple2DPosition(5,5),
+			new Simple2DPosition(4,5),
+		];
+	}
+	getRotationMapping(){
+		return [
+			[[-1,1],[0,0],[1,-1],[2,0]],
+			[[1,1],[0,0],[-1,-1],[0,-2]],
+			[[1,-1],[0,0],[-1,1],[-2,0]],
+			[[-1,-1],[0,0],[1,1],[0,2]],
+		]
+	}
+	
+
+}
+
+export default TetriminoL;
