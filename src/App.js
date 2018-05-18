a81253b70b38b51ea0a4064a0c6d1d656f5a2010
MathieuBouvet <mathieubouvet42@gmail.com>
2018-05-18 13:04:57 +0200

    Add logic methods in App

--- src/App.js
+++ src/App.js
@@ -34,10 +34,166 @@ class App extends Component {
   /** EVENT HANDLERES */
 
   /** LOGIC METHOD */
+  gravity = () => {
+    var newTetetrimino = this.state.tetrimino.clone();
+    const downMove = newTetetrimino.getDownMove();
+    const { isValid, status } = this.isPositionValid(downMove);
+    if(isValid){
+      newTetetrimino.applyPosition(downMove);
+      this.setState({
+        tetrimino: newTetetrimino,
+      });
+    }else if( status === "OVERLAPPING" || status === "OUT_VERTICALLY" ){
+      this.lockTetrimino();
+    }
+  }
   cloneBoard(){
     return this.state.board.map( (elem) => elem.slice() );
   }
 
+  /**
+   * Return if the position is valid and give precision if not
+   * @param  {array}  position the position of a tetrimino
+   * @return {isValid: Boolean, status: string} Object containing a boolean and additional info if position is not valid
+   */
+  isPositionValid(position){
+    //const { board } = this.state;
+    for(let i=0; i<position.length ; i++){
+      let { posX, posY } = position[i];
+      try {
+        if(board[posX][posY] !== "empty"){
+          return { isValid: false, status: "OVERLAPPING" }
+        }
+      }
+      catch(err) {}
+      if(posX > 19 ){
+        return { isValid: false, status: "OUT_VERTICALLY"};
+      }
+      if(posY < 0 || posY > 9){
+        return { isValid: false, status: "OUT_HORIZONTALLY" }
+      }
+    }
+    return { isValid: true, status: "" };
+  }
+
+  lockTetrimino(){
+    const { tetrimino, nextTetrimino } = this.state;
+    let newBoard = this.cloneBoard();
+    for(let i=0 ; i<tetrimino.position.length ; i++){
+      const { posX, posY } = tetrimino.position[i];
+      newBoard[posX][posY] = tetrimino.type;
+    }
+    const linesToDelete = (this.checkNewLines(tetrimino.getRowSpan(), newBoard));
+    if(linesToDelete.length > 0){
+      this.deleteLines(linesToDelete, newBoard)
+    }
+
+    this.setState({
+      board: newBoard,
+      tetrimino: nextTetrimino,
+      nextTetrimino: Tetrimino.getRandom(),
+    });
+
+  }
+  isLineComplete(line){
+    for(let i=0 ; i<line.length ; i++){
+      if(line[i] === "empty"){
+        return false;
+      }
+    }
+    return true;
+  }
+  checkNewLines(rowsIndex, board){
+    let newLines = [];
+    for(let i=0 ; i<rowsIndex.length ; i++){
+      const rowIndex = rowsIndex[i];
+      if(this.isLineComplete(board[rowIndex])){
+        newLines.push(rowIndex)
+      }
+    }
+    return newLines;
+  }
+
+  deleteLines(lines, board){
+    // Deletion of the lines
+    const nbLines = lines.length;
+    for(let i=0 ; i<nbLines ; i++){
+      for(let j=0 ; j<boardSize.row ; j++){
+        board[lines[i]][j] = "empty";
+      }
+    }
+    // Move down above blocks
+    const startingIndex = lines[0]-1;
+    let lineEmpty = false;
+    // Loop through each row above, and stop if there is an empty line detected
+    for(let i=startingIndex ; i >= 0  && !lineEmpty; i--){
+      let nbEmptyBlock = 0;
+      for(let j=0 ; j<boardSize.row ; j++){
+        let currentBlock = board[i][j];
+        if(currentBlock !== "empty"){
+          board[i+nbLines][j] = currentBlock;
+          board[i][j] = "empty";
+        }else{
+          nbEmptyBlock++;
+        }
+      }
+      if(nbEmptyBlock===boardSize.row){
+        lineEmpty = true;
+      }
+    }
+  }
+  moveLeft(){
+    var newTetetrimino = this.state.tetrimino.clone();
+    const leftMovePosition = newTetetrimino.getLeftMove();
+    if(this.isPositionValid(leftMovePosition)["isValid"]){
+      newTetetrimino.applyPosition(leftMovePosition);
+      this.setState({
+        tetrimino: newTetetrimino,
+      });
+    }
+  }
+  moveRight(){
+    var newTetetrimino = this.state.tetrimino.clone();
+    const rightMovePosition = newTetetrimino.getRightMove();
+    if(this.isPositionValid(rightMovePosition)["isValid"]){
+      newTetetrimino.applyPosition(rightMovePosition);
+      this.setState({
+        tetrimino: newTetetrimino,
+      });
+    }
+  }
+  moveDown(){
+    var newTetetrimino = this.state.tetrimino.clone();
+    const downMove = newTetetrimino.getDownMove();
+    const { isValid, status } = this.isPositionValid(downMove);
+    if(isValid){
+      newTetetrimino.applyPosition(downMove);
+      this.setState({
+        tetrimino: newTetetrimino,
+      });
+    }else if( status === "OVERLAPPING" || status === "OUT_VERTICALLY" ){
+      this.lockTetrimino();
+    }
+  }
+  rotateLeft(){
+    var newTetetrimino = this.state.tetrimino.clone();
+    const rotate = newTetetrimino.getLeftRotation();
+    if(this.isPositionValid(rotate)["isValid"]){
+      newTetetrimino.applyLeftRotation(rotate);
+      this.setState({
+        tetrimino: newTetetrimino,
+      });
+    }
+  }
+  rotateRight(){
+    var newTetetrimino = this.state.tetrimino.clone();
+    const rotate = newTetetrimino.getRightRotation();
+    if(this.isPositionValid(rotate)["isValid"]){
+      newTetetrimino.applyRightRotation(rotate);
+      this.setState({
+        tetrimino: newTetetrimino,
+      });
+    }
   }
   render() {
     const { board } = this.state;
