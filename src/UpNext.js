fa4173137896f21cd02a7d76316e46990cfac7da
MathieuBouvet <mathieubouvet42@gmail.com>
2018-05-20 18:56:22 +0200

    Create UpNext component to display next tetrimino

---
+++ src/UpNext.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./UpNext.css";
+
+class UpNext extends Component{
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		
+	}
+}
+
+export default UpNext
