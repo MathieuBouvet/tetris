cee27f1379a799ba4e18771c816a0f9e518b2a91
MathieuBouvet <mathieubouvet42@gmail.com>
2018-05-21 15:59:46 +0200

    Implement UpNext component to show next tetrimino

--- src/UpNext.js
+++ src/UpNext.js
@@ -1,10 +1,30 @@
 import React, { Component } from "react";
 import "./UpNext.css";
 
+import Cell from "./Cell";
+
 class UpNext extends Component{
 
+	shouldComponentUpdate({ tetrimino }){
+		if(tetrimino !== this.props.tetrimino){
+			return true;
+		}
+		return false;
+	}
 	render(){
-		
+		const { tetrimino } = this.props;
+		return (
+			<div className="up-next">{
+				[0,0,0,0].map( (elem, rowIndex) => (
+					[0,0,0,0,0,0].map( (elem, columnIndex) => (
+						<Cell
+							key={rowIndex.toString()+columnIndex.toString()}
+							blockType={tetrimino.isPosition(rowIndex-3,columnIndex+2)?tetrimino.type:"empty"}
+						/>
+					))
+				))
+			}</div>
+		)
 	}
 }
 
