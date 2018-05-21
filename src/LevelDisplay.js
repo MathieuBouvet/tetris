4fdd87ea6eeb3d2676d18448c9b62e34a964d9e3
MathieuBouvet <mathieubouvet42@gmail.com>
2018-05-21 16:25:44 +0200

    Implement basic level display

--- src/LevelDisplay.js
+++ src/LevelDisplay.js
@@ -3,6 +3,14 @@ import "./LevelDisplay.css";
 
 class LevelDisplay extends Component {
 
+	render() {
+		return (
+			<div className="level-display">
+				<div className="level-display-title">Niveau :</div>
+				<div className="level-display-value">{this.props.level}</div>
+			</div>
+		)
+	}
 }
 
 export default LevelDisplay;
