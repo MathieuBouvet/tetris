3ed94e7ff69eab6e5180eac11245c5a82e4e6943
MathieuBouvet <mathieubouvet42@gmail.com>
2018-05-21 16:20:31 +0200

    Implement basic score display component

--- src/ScoreDisplay.js
+++ src/ScoreDisplay.js
@@ -3,6 +3,14 @@ import "./ScoreDisplay.css";
 
 class ScoreDisplay extends Component{
 
+	render(){
+		return (
+			<div className="score-display">
+				<div className="score-display-title">Score :</div>
+				<div className="score-display-value">{this.props.score}</div>
+			</div>
+		)
+	}
 }
 
 export default ScoreDisplay;
