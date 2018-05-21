53e8602c983451db73ece1916a6b170c89ac0e96
MathieuBouvet <mathieubouvet42@gmail.com>
2018-05-21 15:06:11 +0200

    Remove useless constructors in Cell and UpNext components

--- src/Cell.js
+++ src/Cell.js
@@ -2,9 +2,6 @@ import React, { Component } from "react";
 import "./Cell.css";
 
 class Cell extends Component{
-	constructor(props){
-		super(props);
-	}
 
 	shouldComponentUpdate({blockType}){
 		if(blockType===this.props.blockType){
--- src/UpNext.js
+++ src/UpNext.js
@@ -2,9 +2,6 @@ import React, { Component } from "react";
 import "./UpNext.css";
 
 class UpNext extends Component{
-	constructor(props){
-		super(props);
-	}
 
 	render(){
 		
